570de909d2220968f72144fb03875e466c7fc26e
Administrator <Administrator@host.docker.internal>
2019-05-08 18:11:00 +0800

    init

---
+++ src/main/java/com/wz/WzGlassesApplication.java
@@ -0,0 +1,13 @@
+package com.wz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzGlassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzGlassesApplication.class, args);
+	}
+
+}
